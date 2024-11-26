792c3512cf896f11b7df874671a163b02bfce522
Kai Ximenes <kaiximenes@outlook.com>
2024-11-26 17:10:43 -0300

    Primeira aula finalizada

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export const Banner = () => {
+    return ( 
+        <header className='banner'>
+            <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/7570292-varios-jovens-estao-se-reunindo-e-conversando-gratis-vetor.jpg" alt="Banner principal da página do Organo" />
+        </header>
+    )
+}
